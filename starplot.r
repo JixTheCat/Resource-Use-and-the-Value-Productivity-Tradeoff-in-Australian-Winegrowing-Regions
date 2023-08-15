e29ac022d0db437ab0a41d9301cc0e43cdc3b2e8
JixTheCat <bryce.polley@gmail.com>
2023-08-15 17:18:14 +0930

    finished starplots with outputs.

--- starplot.r
+++ starplot.r
@@ -1,48 +1,17 @@
 # Library
 library(fmsb)
 
-# We set the colours we will use:bty
-temp_colors <- c(
-    # Cool
-    "#1E88E5"
-    # Hot
-    , "#D81B60"
-    # Mild
-    , "#004d40"
-    # Warm
-    , "#FFC107"
-)
-rain_colors <- c(
-    # Damp
-    "#1E88E5"
-    # Dry
-    , "#FFC107"
-    # Very Dry
-    , "#D81B60"
-)
-
-# read in the data
-set.seed(100)
-df <- read.csv("df.csv")
-
 # This is to pass to the aggregation function with the
 # preset argument to remove NaNs.
 mean.t <- function(x) {
     mean(x, na.rm = TRUE)
 }
 
-#  We grab the columns we want to display:
-selected_columns <- c("water_used"
-    , "scope1"
-    , "area_harvested"
-    , "average_per_tonne"
-    , "tonnes_grapes_harvested"
-)
-
 starplot <- function(selected_columns, df, target_col, colors_in) {
     dfrescale <- df[, selected_columns]
     dfrescale[, target_col] <- df[, target_col]
-    means <- aggregate(dfrescale, list(df[, target_col]), FUN = mean.t)
+    means <- aggregate(dfrescale[, selected_columns]
+        , list(df[, target_col]), FUN = mean.t)
 
     # The first two rows in the dataframe determine the scaling of the graph.
     # They are set to be the maximum and minimum values
@@ -67,24 +36,69 @@ starplot <- function(selected_columns, df, target_col, colors_in) {
     df_radar <- rbind(df_radar, means[-1, selected_columns])
 
     # And plot!
-    return(
-        radarchart(df_radar
-            , pcol = colors_in
-            , cglwd = 1.2
-            , plwd = 2
-            , plty = 1) +
-        legend(x = 0.7
-            , y = 1
-            , legend = rownames(df_radar[-(1:2), ])
-            , bty = "n"
-            , pch = 20 
-            , col = colors
-            , text.col = "grey"
-            , cex = 1.2
-            , pt.cex = 3)
-    )
+    radarchart(df_radar
+        , pcol = colors_in
+        , cglwd = 1.2
+        , plwd = 2
+        , plty = 1)
+    legend(x = 0.7
+        , y = 1
+        , legend = rownames(df_radar[-(1:2), ])
+        , bty = "n"
+        , pch = 20
+        , col = colors_in
+        , text.col = "grey"
+        , cex = 1.2
+        , pt.cex = 3)
 }
 
+# read in the data
+set.seed(100)
+df <- read.csv("df.csv")
+
+#  We grab the columns we want to display:
+selected_columns <- c("water_used"
+    , "scope1"
+    , "area_harvested"
+    , "average_per_tonne"
+    , "tonnes_grapes_harvested"
+)
+
+# We rename to make the output prettier
+new_columns <- c("Water Used"
+    , "Scope One"
+    , "Area Harvested"
+    , "Average Price Per Tonne"
+    , "Yield"
+)
+
+for (x in seq_len(length(selected_columns))) {
+    names(df)[names(df) == selected_columns[x]] <- new_columns[x]
+}
+
+# We set the colours we will use:bty
+temp_colors <- c(
+    # Cool
+    "#1E88E5"
+    # Hot
+    , "#D81B60"
+    # Mild
+    , "#004d40"
+    # Warm
+    , "#FFC107"
+)
+
+rain_colors <- c(
+    # Damp
+    "#1E88E5"
+    # Dry
+    , "#FFC107"
+    # Very Dry
+    , "#D81B60"
+)
+
 # Two example starplots
-starplot(selected_columns, df, "rain", rain_colors)
-starplot(selected_columns, df, "temp", temp_colors)
+pdf("starplots.pdf")
+starplot(new_columns, df, "rain", rain_colors)
+starplot(new_columns, df, "temp", temp_colors)
+dev.off()
