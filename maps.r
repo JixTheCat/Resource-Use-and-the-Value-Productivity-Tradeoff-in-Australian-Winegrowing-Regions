0b2c4b6a041f5738a0f06cd0c73c251bcf20686a
JixTheCat <bryce.polley@gmail.com>
2023-08-17 21:21:27 +0930

    changed map to income per vineyard by region

--- maps.r
+++ maps.r
@@ -1,59 +1,50 @@
 #######################
 # Lets make some maps #
+
+library(ggplot2)
+library(gridExtra)
+library(viridis)
+library(sf)
+library(cowplot)
+library(stringi)
+
+mean.t <- function(x) {
+    mean(x, na.rm = TRUE)
+}
+
 nt <- read.csv("no_trans.csv")
 v <- st_read("data/Wine_Geographical_Indications_Australia_2022/Wine_Geographical_Indications_Australia_2022.shp")
 aus <- st_read("data/STE_2021_AUST_SHP_GDA2020/STE_2021_AUST_GDA2020.shp")
 
-v$ha_tonnes_grapes_harvested <- 0
-v$ha_value <- 0
+v$value <- 0
 
-aggs.ha_tonnes_grapes_harvested <- aggregate(
-    nt$ha_tonnes_grapes_harvested * 10000
+agg_value <- aggregate(
+    nt$value
     , list(nt$giregion), FUN = mean.t)
-aggs.ha_value <- aggregate(
-    nt$ha_value
-    , list(nt$giregion)
-    , FUN=mean.t)
 
-for (region in unique(df$giregion)){
+agg_value$x <- agg_value$x / 1000000
+
+for (region in unique(nt$giregion)){
     v[v$GI_NAME == region
-    , "ha_tonnes_grapes_harvested"] <- aggs.ha_tonnes_grapes_harvested[
-    aggs.ha_tonnes_grapes_harvested$Group.1 == region
+    , "value"] <- agg_value[
+    agg_value$Group.1 == region
     , ]$x
-    v[
-        v$GI_NAME==region
-        , "ha_value"] <- aggs.ha_value[
-            aggs.ha_value$Group.1 == region
-            , ]$x
 }
 
 # note these are the averages.
-#ggdraw() + 
 value_map <- ggplot() +
-    ggtitle("Average Price Per Ha ") +
-    geom_sf(data = v, aes(fill = ha_value)) +
+    ggtitle("Average Vineyard Income by Region") +
+    geom_sf(data = v, aes(fill = value)) +
     geom_sf(data=aus, fill = NA) +
     xlim(113, 154) +
     ylim(-45, -25) +
-    scale_fill_gradientn(colours=rev(viridis(3))
-                         , name=""
+    scale_fill_gradientn(colours = rev(viridis(3))
+                         , name = ""
                          , na.value = "grey100") +
     theme_void()
 
-yield_map <-  ggplot() +
-    xlim(113, 154) +
-    ylim(-45, -25) +
-    theme_void() +
-        ggtitle("Average Yield (tonnes) Per Ha") +
-    geom_sf(data = v
-        , aes(fill = ha_tonnes_grapes_harvested)) +
-    geom_sf(data=aus, fill = NA) +
-    scale_fill_gradientn(colours=rev(viridis(3))
-                         , name=""
-                         , na.value = "grey100")
-
 pdf("my_map.pdf")
-grid.arrange(value_map, yield_map, ncol=2, nrow=1)
+print(value_map)
 dev.off()
 
 # below is just a way to grab the coef from a linear model.
